34f63f8048ce55c5973a64b4e40fc10ef1a4b7b4
Mike Yorke <mike@Mikes-MacBook-Pro.local>
2017-11-21 21:03:26 -0800

    Local Development

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+(function($) {
+  "use strict";
+  // Main Menu Toggle
+  // $('header .menu-toggle').on("click", function() {
+  //   $('body').toggleClass('main-menu-open');
+  // });
+
+  // Closes Main Menu when user clicks outside
+  // $('#header').on('click', function(event) {
+  //   if (!$(event.target).closest('.menu-toggle').length) {
+  //     $('body').removeClass('main-menu-open');
+  //   }
+  // });
+
+  $.stellar({
+    horizontalScrolling: false
+  });
+
+
+
+})(jQuery);
